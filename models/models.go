80309e38857bb191d1b62d7cd3ae6ecab6e642e6
Prakash Joshi <prakash.joshi167@hotmail.com>
2024-06-06 23:04:53 +0530

    update Todo struct to map id for json

--- models/models.go
+++ models/models.go
@@ -3,7 +3,7 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Todo struct {
-	ID        primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Completed bool               `json:"completed"`
 	Body      string             `json:"body"`
 }
